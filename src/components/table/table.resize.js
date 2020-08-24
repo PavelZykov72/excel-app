f5005dbb3f45bfad2e322743f6c4a96aab2762de
Павел Зыков <pavel.zykov.work@gmail.com>
2020-08-24 22:04:48 +0500

    Resize table

---
+++ src/components/table/table.resize.js
@@ -0,0 +1,68 @@
+import { $ } from '@/core/DOM';
+
+export function tableResizeHandler($root, event) {
+    const { resize } = event.target.dataset;
+
+    const $resizer = $(event.target);
+    const $parent = $resizer.closest('[data-type="resizable"]');
+    const coords = $parent.getCoords();
+
+    if (resize === 'column') {
+        $resizer.css({
+            bottom: '-100vh',
+        });
+
+        const { colIndex: index } = $parent.data;
+        const $cells = $root.findAll(`[data-col-index="${index}"]`);
+
+        let value;
+        document.onmousemove = (e) => {
+            const delta = e.pageX - coords.right;
+            value = coords.width + delta;
+
+            $resizer.css({
+                right: `${-delta}px`,
+            });
+        };
+
+        document.onmouseup = () => {
+            document.onmousemove = null;
+            document.onmouseup = null;
+
+            $cells.forEach((el) => el.style.width = `${value}px`);
+            $parent.css({
+                width: `${value}px`,
+            });
+            $resizer.css({
+                bottom: '',
+                right: '',
+            });
+        };
+    } else if (resize === 'row') {
+        $resizer.css({
+            right: '-100vw',
+        });
+
+        let value;
+        document.onmousemove = (e) => {
+            const delta = e.pageY - coords.bottom;
+            value = coords.height + delta;
+            $resizer.css({
+                bottom: `${-delta}px`,
+            });
+        };
+
+        document.onmouseup = () => {
+            document.onmousemove = null;
+            document.onmouseup = null;
+
+            $parent.css({
+                height: `${value}px`,
+            });
+            $resizer.css({
+                bottom: '',
+                right: '',
+            });
+        };
+    }
+}
