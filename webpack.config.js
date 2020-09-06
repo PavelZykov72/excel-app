80fe3004c99ce4d9b8b2774ca7092c4ad386c172
Павел Зыков <pavel.zykov.work@gmail.com>
2020-09-06 23:13:39 +0500

    Eslint fix

--- webpack.config.js
+++ webpack.config.js
@@ -10,9 +10,9 @@ const webpack = require('webpack');
 const isProduction = process.env.NODE_ENV === 'production';
 const isDevelopment = process.env.NODE_ENV === 'development';
 
-const filename = (extension) => isDevelopment ?
-                            `bundle.${extension}` :
-                            `bundle.[hash].${extension}`;
+const filename = (extension) => isDevelopment
+                              ? `bundle.${extension}`
+                              : `bundle.[hash].${extension}`;
 
 const getJsLoaders = () => {
   const loaders = [ {
